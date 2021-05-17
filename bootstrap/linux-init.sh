c3c8a99f13d0532a1010ae193941583fa40c233a
distkloc <rectpali+ws@gmail.com>
2021-05-17 14:30:15 +0900

    Add libffi-dev

--- bootstrap/linux-init.sh
+++ bootstrap/linux-init.sh
@@ -9,9 +9,5 @@ sudo apt install -y \
   peco \
   neovim \
   dirmngr gpg \
-  unzip
-
-
-# poetry
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python -
-$HOME/.local/bin/poetry config virtualenvs.in-project true
+  unzip \
+  libffi-dev
