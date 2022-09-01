488e7f794e11bee6b1511ca931fa507140911ea9
distkloc <rectpali+ws@gmail.com>
2022-09-02 00:15:56 +0900

    Remove prezto resources

--- bootstrap/unix-deploy.sh
+++ bootstrap/unix-deploy.sh
@@ -16,10 +16,3 @@ ln -fnsv ${DOT_PATH%/}/.vimrc ~/.config/nvim/init.vim
 
 # vim directory
 mkdir -p {~/.vim/backup,~/.vim/swap,~/.vim/undo,~/.vim/viminfo}
-
-# prezto
-if [ -x "$(command -v zsh)" ] && [ -d ~/.zprezto ]; then
-
-  # https://stackoverflow.com/questions/33665820/how-to-execute-zsh-shell-commands-in-bash-script
-  zsh -c ${DOT_PATH%/}/bootstrap/prezto-deploy.zsh
-fi
